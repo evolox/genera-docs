4bdcf927a46d41e518fb9b7c736f172d69400323
Alexey Kupriyanenko <alexeyk@wix.com>
2024-04-08 23:08:22 +0100

    remove build

--- build/assets/js/ee5237ea.6939ba77.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgenera_docs=self.webpackChunkgenera_docs||[]).push([[667],{1966:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
