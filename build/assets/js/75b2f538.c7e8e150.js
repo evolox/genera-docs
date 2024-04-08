69a2ca659176731e844917f9ee6b653bff9dddf1
Alexey Kupriyanenko <alexeyk@wix.com>
2024-04-08 23:02:02 +0100

    add build to the repo

---
+++ build/assets/js/75b2f538.c7e8e150.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgenera_docs=self.webpackChunkgenera_docs||[]).push([[465],{2945:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ build/assets/js/e16015ca.2e60680b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgenera_docs=self.webpackChunkgenera_docs||[]).push([[878],{9635:a=>{a.exports=JSON.parse('{"label":"hola","permalink":"/blog/tags/hola","allTagsPath":"/blog/tags","count":1,"unlisted":false}')}}]);
